f01131a6ab99577452b383f2fed616dbd456b40e
vit <vitaly.lobatsevich@gmail.com>
2020-09-07 17:56:56 +0300

    Base project.

---
+++ src/main/java/by/vit/roomswithbulbs/RoomsWithBulbsApplication.java
@@ -0,0 +1,13 @@
+package by.vit.roomswithbulbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomsWithBulbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomsWithBulbsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/vit/roomswithbulbs/RoomsWithBulbsApplicationTests.java
@@ -0,0 +1,13 @@
+package by.vit.roomswithbulbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomsWithBulbsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

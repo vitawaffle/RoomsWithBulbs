4b3fb828cd5106e2fc0e561691bdb6ac29efbdd0
Vitaly Lobatsevich <sir.nakheel@yandex.by>
2020-09-11 08:52:28 +0300

    Room view interactivity

--- src/main/resources/static/js/room.js
+++ src/main/resources/static/js/room.js
@@ -1,43 +1,62 @@
 const id = new URLSearchParams(window.location.search).get("id");
+const switchLightButton = $("#switchLight");
+const lightStatusText = $("#lightStatus");
 
 let stompClient = null;
+
+function updateRoom(room) {
+    lightStatusText.html(room.light ? "on" : "off");
+    switchLightButton.html(room.light ? "Off" : "On");
+}
+
+function loadRoom() {
+    $.getJSON(`/rooms/${id}`, function (room) {
+        document.title = room.name;
+        updateRoom(room);
+    });
+}
 
 function connect() {
     let socket = new SockJS("/rooms-with-bulbs");
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
+
         /* Start of debug section */
         console.log(`Connected: ${frame}`);
         /* End of debug section */
 
-        stompClient.subscribe(`/topic/room/${id}`, function (room) {
+        stompClient.subscribe(`/topic/room/${id}`, function (data) {
+
             /* Start of debug section */
-            console.log(JSON.parse(room.body));
+            console.log(JSON.parse(data.body));
             /* End of debug section */
+
+            switchLightButton.prop("disabled", false);
+            updateRoom(JSON.parse(data.body));
         });
     });
 }
 
 function disconnect() {
-    if (stompClient !== null) {
-        stompClient.disconnect();
-    }
 
     /* Start of debug section */
     console.log("Disconnected");
     /* End of debug section */
+
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
 }
 
 function switchLight() {
     stompClient.send(`/app/switchLight/${id}`, {}, {});
 }
 
-function loadRoom() {
-    $.getJSON(`/rooms/${id}`, function (room) {
-        document.title = room.name;
-    });
-}
+switchLightButton.click(function () {
+    switchLight();
+});
 
 window.onload = function () {
     loadRoom();
+    connect();
 }
